c2a912e948cfb7e1c87ed0e5239c7fce4de448c4
Akash <akashsardar383@gmail.com>
2023-06-26 23:28:50 +0530

    ok

--- src/main/java/com/akash/ecommerce/entity/CartItem.java
+++ src/main/java/com/akash/ecommerce/entity/CartItem.java
@@ -1,14 +1,15 @@
 package com.akash.ecommerce.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/com/akash/ecommerce/security/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.akash.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setUserDetailsService(userDetailsService());
+		return authenticationProvider;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public SecurityFilterChain securityfilterChain(HttpSecurity http) throws Exception {
+		http
+			.cors()
+			.and()
+			.csrf()
+			.disable()
+			.authorizeRequests()
+			.requestMatchers(HttpMethod.POST, "/api/v1/products").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.DELETE, "/api/v1/products/*").hasRole("ADMIN") 
+			.requestMatchers(HttpMethod.PUT, "/api/v1/products/*").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.POST, "/api/v1/categories").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.DELETE, "/api/v1/categories/*").hasRole("ADMIN") 
+			.requestMatchers(HttpMethod.PUT, "/api/v1/categories/*").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.POST, "/api/v1/suppliers").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.DELETE, "/api/v1/suppliers/*").hasRole("ADMIN") 
+			.requestMatchers(HttpMethod.PUT, "/api/v1/suppliers/*").hasRole("ADMIN")
+			.and()
+			.httpBasic()
+			.and()
+			.logout();
+		
+		return http.build();
+	}
+}
